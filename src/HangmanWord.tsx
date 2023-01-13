587a1ab7120543e8f31fd5dae9abe66e80c86d5c
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-13 21:38:32 +0100

    Letters part added

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,5 +1,18 @@
 export function HangmanWord() {
 
+    const word = 'test';
+    const guessedLetters = ['t'];
 
+    return <div style={{ display: 'flex', gap: '1rem', fontSize: '5rem', fontWeight: '700', textTransform: 'uppercase', fontFamily: 'Nunito' }}>
+        
+        {word.split('').map((letter, i) => (
+            <span style={{ borderBottom: '.5rem solid #5D6D7E', borderRadius: '1rem' }} key={i}>
+
+                <span style={{ visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden' }}>{letter}</span>
+
+            </span>
+        ))}
+
+    </div>
 
 }
