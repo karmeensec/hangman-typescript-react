572a5d9093429fdd8e25a35ee10c8648be75cc97
kamilismayilzade <kamil.ismayilzade.2016@mail.ru>
2023-01-13 21:18:06 +0100

    Font Family Nunito used

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { useState } from "react"
+import words from './comWordList.json'
+
+import { HangmanPicture } from "./HangmanPicture";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
 
 
 function App() {
-  
-  return <h1>Hi</h1>
+
+  const [guessWord, setGuessWord] = useState( ()=> {
+    return words[Math.floor(Math.random() * words.length)]; // random guess
+  } );
+
+  const [guessedLetter, setGuessedLetter] = useState<string[]>([]);
+
+  // Game TSX
+
+  return <div style={{ maxWidth: '900px', display: 'flex', flexDirection: 'column', gap: '2rem', margin: '0 auto', alignItems: 'center' }}>
+
+    <div style={{fontSize: '2rem', textAlign: 'center', fontFamily: 'Nunito'}}>Lost Won</div>
+
+    <HangmanPicture />
+    <HangmanWord />
+    <Keyboard />
+
+  </div>
 
 }
 
